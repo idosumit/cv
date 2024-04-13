ccac220f0f27bc2fcbd38ec2f6fcdaaa227785cc
Sumit <psumitcoding@gmail.com>
2024-04-13 21:48:42 +0900

    updated resume data

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -1,3 +1,22 @@
+import {
+  AmbitLogo,
+  BarepapersLogo,
+  BimLogo,
+  CDGOLogo,
+  ClevertechLogo,
+  ConsultlyLogo,
+  EvercastLogo,
+  Howdy,
+  JarockiMeLogo,
+  JojoMobileLogo,
+  Minimal,
+  MobileVikingsLogo,
+  MonitoLogo,
+  NSNLogo,
+  ParabolLogo,
+  TastyCloudLogo,
+  YearProgressLogo,
+} from "@/images/logos";
 import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
 
 export const RESUME_DATA = {
